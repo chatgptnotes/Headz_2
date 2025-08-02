871ffc97d47ef253d4a121ac5afd992c49d2d520
Aman Bhondge <amanbhondge@gmail.com>
2025-08-02 18:42:54 +0530

    A3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
-import TryNow from './pages/trynow/Trynow'
+import TryNow from './pages/trynow/TryNow'
 
 const App = () => {
   return (
